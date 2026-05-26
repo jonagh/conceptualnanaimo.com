8c370220a942c97ffb2d673bd1e090dfc57cae7c
jonagh <jonagh@gmail.com>
2026-05-26 08:49:57 -0700

    add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,51 +4,59 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig, fontProviders } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-	redirects: {
-		'/blog': '/',
+  redirects: {
+      '/blog': '/',
 	},
-	site: 'https://conceptualnanaimo.com',
-	integrations: [mdx(), sitemap()],
-	fonts: [
-		{
-			provider: fontProviders.local(),
-			name: 'Atkinson',
-			cssVariable: '--font-atkinson',
-			fallbacks: ['sans-serif'],
-			options: {
-				variants: [
-					{
-						src: ['./src/assets/fonts/atkinson-regular.woff'],
-						weight: 400,
-						style: 'normal',
-						display: 'swap',
-					},
-					{
-						src: ['./src/assets/fonts/atkinson-bold.woff'],
-						weight: 700,
-						style: 'normal',
-						display: 'swap',
-					},
-				],
-			},
-		},
-		{
-			provider: fontProviders.local(),
-			name: 'Architects Daughter',
-			cssVariable: '--font-architectural',
-			fallbacks: ['sans-serif'],
-			options: {
-				variants: [
-					{
-						src: ['./src/assets/fonts/ArchitectsDaughter-Regular.ttf'],
-						weight: 400,
-						style: 'normal',
-						display: 'swap',
-					},
-				],
-			},
-		},
+
+  site: 'https://conceptualnanaimo.com',
+  integrations: [mdx(), sitemap()],
+
+  fonts: [
+      {
+          provider: fontProviders.local(),
+          name: 'Atkinson',
+          cssVariable: '--font-atkinson',
+          fallbacks: ['sans-serif'],
+          options: {
+              variants: [
+                  {
+                      src: ['./src/assets/fonts/atkinson-regular.woff'],
+                      weight: 400,
+                      style: 'normal',
+                      display: 'swap',
+                  },
+                  {
+                      src: ['./src/assets/fonts/atkinson-bold.woff'],
+                      weight: 700,
+                      style: 'normal',
+                      display: 'swap',
+                  },
+              ],
+          },
+      },
+      {
+          provider: fontProviders.local(),
+          name: 'Architects Daughter',
+          cssVariable: '--font-architectural',
+          fallbacks: ['sans-serif'],
+          options: {
+              variants: [
+                  {
+                      src: ['./src/assets/fonts/ArchitectsDaughter-Regular.ttf'],
+                      weight: 400,
+                      style: 'normal',
+                      display: 'swap',
+                  },
+              ],
+          },
+      },
 	],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
